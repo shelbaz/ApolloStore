8f1b5bd46a9e683ff2cd9e17af37f9a86010f4e0
Radu Raicea <radu@raicea.com>
2017-09-18 12:51:34 -0400

    change comment symbol in create.sql

--- postgres/create.sql
+++ postgres/create.sql
@@ -1,7 +1,7 @@
-# -----------------------------------------------------------------------
-# This file is run when the postgres container is first created. It
-# creates 3 databases, one for each configuration.
-# -----------------------------------------------------------------------
+-- -----------------------------------------------------------------------
+-- This file is run when the postgres container is first created. It
+-- creates 3 databases, one for each configuration.
+-- -----------------------------------------------------------------------
 
 CREATE DATABASE db_dev;
 CREATE DATABASE db_prod;
