7d4d01113f7eb45ce037e61f73e50399ad48950d
Radu-Raicea <radu@raicea.com>
2017-09-26 00:45:41 -0400

    reverse order of dropping of tables

--- flask/project/models/__init__.py
+++ flask/project/models/__init__.py
@@ -44,37 +44,38 @@ def create_tables():
         time.sleep(5)
         create_tables()
 
-    # Calls the class functions that create the tables of the corresponding models
-    def drop_tables():
-        try:
-            from project.models.auth_model import User
-            User.drop_table()
 
-            from project.models.item_model import Item
-            Item.drop_table()
+# Calls the class functions that create the tables of the corresponding models
+def drop_tables():
+    try:
+        from project.models.television_model import Television
+        Television.drop_table()
 
-            from project.models.inventory_model import Inventory
-            Inventory.drop_table()
+        from project.models.tablet_model import Tablet
+        Tablet.drop_table()
 
-            from project.models.cart_model import Cart
-            Cart.drop_table()
+        from project.models.monitor_model import Monitor
+        Monitor.drop_table()
 
-            from project.models.desktop_model import Desktop
-            Desktop.drop_table()
+        from project.models.laptop_model import Laptop
+        Laptop.drop_table()
 
-            from project.models.laptop_model import Laptop
-            Laptop.drop_table()
+        from project.models.desktop_model import Desktop
+        Desktop.drop_table()
 
-            from project.models.monitor_model import Monitor
-            Monitor.drop_table()
+        from project.models.cart_model import Cart
+        Cart.drop_table()
 
-            from project.models.tablet_model import Tablet
-            Tablet.drop_table()
+        from project.models.inventory_model import Inventory
+        Inventory.drop_table()
 
-            from project.models.television_model import Television
-            Television.drop_table()
+        from project.models.item_model import Item
+        Item.drop_table()
 
-        except Exception:
-            # Safeguards against the first time creating the Docker volume, where postgres/create.sql didn't finish running
-            time.sleep(5)
-            drop_tables()
+        from project.models.auth_model import User
+        User.drop_table()
+
+    except Exception:
+        # Safeguards against the first time creating the Docker volume, where postgres/create.sql didn't finish running
+        time.sleep(5)
+        drop_tables()
