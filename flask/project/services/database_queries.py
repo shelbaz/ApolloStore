0b8d10f78648e0421526fa5e8a1680468dfb3cf6
Shawn Elbaz <shawnelbaz@shawns-mbp-3.wireless.concordia.ca>
2017-09-28 01:23:49 -0400

    created db queries, connected view with db calls and forms

--- flask/project/services/database_queries.py
+++ flask/project/services/database_queries.py
@@ -1,4 +1,8 @@
 from project.models.auth_model import User
+from project.models.item_model import Item
 
 def get_all_users():
     return User.query_filtered_by()
+
+def get_all_items():
+    return Item.query_filtered_by()
--- flask/project/website/views.py
+++ flask/project/website/views.py
@@ -4,10 +4,11 @@
 
 from flask import jsonify, render_template, Blueprint, g, request, abort, redirect
 from project.services.authentication import create_user
+from project.services.electronics import create_desktop, create_television, create_tablet, create_monitor, create_laptop
 from project import logger
 from project.models.auth_model import User
 from flask_login import login_required, current_user, login_user, logout_user
-from project.services.database_queries import get_all_users
+from project.services.database_queries import get_all_users, get_all_items
 
 website_blueprint = Blueprint('website_blueprint', __name__)
 
@@ -32,11 +33,105 @@ def test():
     return render_template('test.html')
 
 
-@website_blueprint.route('/dashboard')
+@website_blueprint.route('/dashboard', methods=["GET", "POST"])
 #@auth.login_required
 def dashboard():
-    return render_template('dashboard.html', users= get_all_users())
+    if request.method == "POST" and request.form['desktopdimensions']:
+        price = request.form['price']
+        weight = request.form['weight']
+        brand = request.form['brand']
+        processor = request.form['processor']
+        ramsize = request.form['ram_size']
+        cpucores = request.form['cpu_cores']
+        hdsize = request.form['hd_size']
+        dimensions = request.form['desktopdimensions']
 
+        if price and weight and brand and processor and ramsize and cpucores and hdsize and dimensions:
+            desktop = create_desktop(brand, price, weight, processor, ramsize, cpucores, hdsize, dimensions)
+
+            if desktop:
+                return render_template('dashboard.html'), 201
+            else:
+                logger.error('couldnt create desktop item')
+
+    if request.method == "POST" and request.form['laptop_display_size']:
+
+        price = request.form['price']
+        weight = request.form['weight']
+        brand = request.form['brand']
+        processor = request.form['processor']
+        ramsize = request.form['ram_size']
+        cpucores = request.form['cpu_cores']
+        hdsize = request.form['hd_size']
+        operatingsystem = request.form['operating_system']
+        displaysize = request.form['laptop_display_size']
+        touchscreen = request.form['touchscreen']
+        camera = request.form['camera']
+        battery = request.form['battery']
+
+        if price and weight and brand and processor and ramsize and cpucores and hdsize and displaysize:
+            laptop = create_laptop(brand, price, weight, displaysize, processor, ramsize, cpucores, hdsize, battery, operatingsystem, touchscreen, camera)
+
+            if laptop:
+                return render_template('dashboard.html'), 201
+            else:
+                logger.error('couldnt create laptop item')
+
+    if request.method == "POST" and request.form['tablet_display_size']:
+
+        price = request.form['price']
+        weight = request.form['weight']
+        brand = request.form['brand']
+        processor = request.form['processor']
+        ramsize = request.form['ram_size']
+        cpucores = request.form['cpu_cores']
+        hdsize = request.form['hd_size']
+        operatingsystem = request.form['operating_system']
+        displaysize = request.form['tablet_display_size']
+        camera = request.form['camera']
+        battery = request.form['battery']
+        dimensions = request.form['dimensions']
+
+        if price and weight and brand and processor and ramsize and cpucores and hdsize and displaysize:
+            tablet = create_tablet(brand, price, weight, displaysize, dimensions, processor, ramsize, cpucores, hdsize, battery, os, camera)
+
+            if tablet:
+                return render_template('dashboard.html'), 201
+            else:
+                logger.error('couldnt create tablet item')
+
+    if request.method == "POST" and request.form['monitor_dimensions']:
+
+        price = request.form['price']
+        weight = request.form['weight']
+        brand = request.form['brand']
+        dimensions = request.form['monitor_dimensions']
+
+        if price and weight and brand and dimensions:
+            monitor = create_monitor(brand, price, weight, dimensions)
+
+            if monitor:
+                return render_template('dashboard.html'), 201
+            else:
+                logger.error('couldnt create monitor item')
+
+    if request.method == "POST" and request.form['tv_dimensions']:
+
+        price = request.form['price']
+        weight = request.form['weight']
+        brand = request.form['brand']
+        dimensions = request.form['tv_dimensions']
+        tvtype = request.form['tv_type']
+
+        if price and weight and brand and dimensions:
+            television = create_television(brand, price, weight, tvtype, dimensions)
+
+            if television:
+                return render_template('dashboard.html'), 201
+            else:
+                logger.error('couldnt create tv item')
+
+    return render_template('dashboard.html', users=get_all_users(), items=get_all_items())
 
 
 # Registers a new user
