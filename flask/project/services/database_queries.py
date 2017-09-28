bf0cda8e2742ee75d465473de17d962752bb0326
Radu-Raicea <radu@raicea.com>
2017-09-28 11:14:30 -0400

    create services for getting electronics and increasing their count

--- flask/project/services/database_queries.py
+++
@@ -1,8 +0,0 @@
-from project.models.auth_model import User
-from project.models.item_model import Item
-
-def get_all_users():
-    return User.query_filtered_by()
-
-def get_all_items():
-    return Item.query_filtered_by()
