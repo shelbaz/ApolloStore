4863057b768441f4b0154f31a575633ab8a47866
r <radu@raicea.com>
2017-11-08 14:45:44 -0500

    make identity map a singleton

--- flask/project/identityMap.py
+++ flask/project/identityMap.py
@@ -2,7 +2,15 @@
 from project.aspect import Aspect
 
 
-class IdentityMap():
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class IdentityMap(metaclass=Singleton):
 
     def __init__(self):
         self.dict = {}
